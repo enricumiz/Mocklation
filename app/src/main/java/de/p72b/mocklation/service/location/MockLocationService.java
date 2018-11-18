b290dcef253db9f221bf10e10047cb3cc53f5902
p72b <willy.riechert@carjump.de>
2018-11-18 16:49:54 +0100

    Deleted deprecated MainActivity and dependencies.

--- app/src/main/java/de/p72b/mocklation/service/location/MockLocationService.java
+++ app/src/main/java/de/p72b/mocklation/service/location/MockLocationService.java
@@ -23,7 +23,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
-import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
